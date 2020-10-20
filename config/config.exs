28cea6386af4add7c1f9df421b20973820cd80ba
Che <ihor.chernov@infotech.gov.ua>
2020-10-20 16:29:47 +0300

    config settings

--- config/config.exs
+++ config/config.exs
@@ -7,16 +7,16 @@ config :n2o,
   ttl: 60 * 60 * 4,
   nitro_prolongate: false,
   mqtt_services: [:erp, :wms],
-  #ws_services: [:chat, :crm],
+  #ws_services: [],
   tables: [:cookies, :file, :caching, :ws, :mqtt, :tcp, :async, :track],
   upload: "./priv/storage",
-  protocols: [:nitro_n2o, :n2o_heart]#,
-  #routes: CRM.Routes
+  protocols: [:nitro_n2o, :n2o_heart],
+  routes: Tpage.Routes
 
 config :kvs,
   dba: :kvs_rocks,
   dba_st: :kvs_st,
-  schema: [:kvs, :kvs_stream, :bpe_metainfo]
+  schema: [:kvs, :kvs_stream]
 
 config :form,
   module: :form2
