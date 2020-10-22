488162f6a6b3b4397907e6f9388ee29d3850a97a
Che <ihor.chernov@infotech.gov.ua>
2020-10-22 13:05:21 +0300

    added insert_before

--- lib/tpage/index.ex
+++ lib/tpage/index.ex
@@ -22,6 +22,20 @@ defmodule Tpage.Index do
   def event({:append_rows, tableId, readerId}) do
     IO.inspect("UPPEND EVENT")
     show_rows('/points', readerId)
+    :nitro.wire("set_height();")
+  end
+
+  def event({:append_row_before, tId, rId}) do
+    d = :kvs.take(KVS.reader(:kvs.load_reader(rId), args: 1, dir: 1))
+    :kvs.save(d)
+    KVS.reader(args: rows) = d
+    for tr <- rows do
+      row = NITRO.tr(cells: [
+        NITRO.td(body: :nitro.to_binary(elem(tr, 1))),
+        NITRO.td(body: :nitro.to_binary("point"))
+      ])
+      :nitro.insert_top(:table, row)
+    end
   end
 
   def event(mess) do
@@ -29,14 +43,14 @@ defmodule Tpage.Index do
   end
 
   def show_rows(feed, rid) do
-    d = :kvs.take(KVS.reader(:kvs.load_reader(rid), args: 10, dir: 0))
+    d = :kvs.take(KVS.reader(:kvs.load_reader(rid), args: 1, dir: 0))
     :kvs.save(d)
     KVS.reader(args: rows, id: rid) = d
 
     for r <- rows do
       row = NITRO.tr(cells: [
         NITRO.td(body: :nitro.to_binary(elem(r,1))),
-        NITRO.td(body: :nitro.to_binary("point")),
+        NITRO.td(body: :nitro.to_binary("point"))
       ])
       :nitro.insert_bottom(:table, row)
     end
--- priv/static/js/table.js
+++ priv/static/js/table.js
@@ -8,22 +8,55 @@ qi('divTable').addEventListener("scroll", function(e){
 
 function update_table(scrll){
   r = qi('table')['data-rid'];
-  console.log([scrll, 10 + scrll + qi('divTable').offsetHeight, qi('table').offsetHeight ]);
+  dt = qi('divTable');
+  acc = qi('table')['data-scroll']
+  console.log([scrll, qi('divTable').offsetHeight, qi('table').offsetHeight ]);
   
+  // move down
   if(scrll + qi('divTable').offsetHeight + 11 > qi('table').offsetHeight ){
     direct(tuple(atom('append_rows'), string('table'), string(r)))
   }
-  
+
+  // move up
+  if(scrll < acc){
+    direct(tuple(atom('append_row_before'), string('table'), string(r)))
+    t.childNodes.forEach(tr => {
+      if(tr.getBoundingClientRect().y > scrll + dt.clientHeight){
+        t.removeChild(tr)
+      }}
+    );
+    console.log('MOVE_BACK');
+  }
+
+  set_height();
+  qi('table')['data-scroll'] = scrll;
 }
 
 function set_height(){
   r = qi('table')['data-rid'];
+  t = qi('table');
   div = qi('divTable').clientHeight;
-  table = qi('table').clientHeight;
+  table = t.clientHeight;
+  console.log(["#", div, table]);
   if(div >= table){
     direct(tuple(atom('append_rows'), string('table'), string(r)))
   }
   
-  qi('divTable').height = qi('table').clientHeight - 10;
-  console.log([qi('table').clientHeight, qi('divTable').clientHeight])
+  qi('divTable').height = t.clientHeight - 10;
+  //console.log([t.clientHeight, qi('divTable').clientHeight]);
+
+  tH = t.clientHeight;
+  tScrl = t.scrollTop;
+  dt = t.clientHeight;
+
+  // t.childNodes.forEach(tr => {
+  //   if(tr.getBoundingClientRect().y < 0){
+  //     t.removeChild(tr)
+  //   }}
+  // );
+  
+  console.log([tH, tScrl, dt]);
+  // if(tH){
+
+  // }
 }
